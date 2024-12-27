e231e78f4f8af7d2521bee6d981b88cbd5f0ca0d
César Augusto Vieira de Andrade <111445273+xMortalHazardx@users.noreply.github.com>
2024-12-26 22:35:45 -0300

    Automation Atualizado
    Atualizado e funcionando.

--- Python-Exercicios/Automation.py
+++ Python-Exercicios/Automation.py
@@ -6,14 +6,15 @@ from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 #template para TKInter
 
 
-def abrirJS():
-    with open('/home/machine/Documents/Python/Files/credentials.json','r') as arquivo:
-        todo = json.load(arquivo)
-    return todo 
+# def abrirJS():
+#     with open('/home/machine/Documents/Python/Files/credentials.json','r') as arquivo:
+#         todo = json.load(arquivo)
+#     return todo 
 
 #root= tk.Tk()
 #root.title("Chamado Passivo")
@@ -26,8 +27,7 @@ while True:
     
     
     options = webdriver.ChromeOptions()
-    #options.add_argument("--headless=new")
-    options.add_argument('window-size=1200x600')
+    #options.add_argument("--headless=new")    
     driver = webdriver.Chrome(options=options)
 
 
@@ -36,11 +36,11 @@ while True:
 
     driver.get(url)
     time.sleep(1)
-    buton = driver.find_elements(By.ID,"UserName")
 
-    buton[0].send_keys("cesar.emc")
+    buton = driver.find_elements(By.ID,"UserName")
+    buton[0].send_keys("Usuario")
     buton = driver.find_elements(By.ID,"Key")
-    buton[0].send_keys("!Caskthebest14")
+    buton[0].send_keys("PassSenha")
 
 
     buton = driver.find_elements(By.ID,"btnContinuar")
@@ -54,10 +54,21 @@ while True:
     buton = driver.find_elements(By.ID,"abrirSelecaoSolicitante")
     buton[0].click()
 
-    buton = driver.find_elements(By.LINK_TEXT,"k-grid-filter")
-    buton[0].click()
-    
-    time.sleep(2)
+    wait = WebDriverWait(driver, 5)
+    icon = wait.until(EC.element_to_be_clickable((By.XPATH, "//th[@data-field='Login']//span[@class='k-icon k-filter']")))
+    # Clica no ícone
+    icon.click()
+    time.sleep(5)
 
+    buton = driver.find_element(By.XPATH, "/html/body/div[10]/form/div[1]/input")
+    buton.send_keys("leoncio.faria")
+    time.sleep(5)
 
+    buton = driver.find_element(By.XPATH, "//button[@class='k-button k-primary']").click()
+
+    icon = driver.find_element(By.XPATH, "//td//span[@class='k-icon k-i-tick']/parent::a")    
+    icon.click()
+    time.sleep(10)
     driver.quit()
+    
+    break
